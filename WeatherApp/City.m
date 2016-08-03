66141a1baa72550bb854525f27f919b9f62543b3
Teodora <teodora.garashanin@gmail.com>
2016-08-03 19:49:11 +0200

    WeatherApp

--- WeatherApp/City.m
+++ WeatherApp/City.m
@@ -11,42 +11,29 @@
 
 @implementation City
 
-
 #pragma mark - Designated Initializer
 
 - (instancetype)initWithName:(NSString *)name andLongitude: (double) longitude andLatitude: (double) latitude {
-    
     if (self = [super init]) {
-        
-        
         self.name = name;
         self.latitude = latitude;
         self.longitude = longitude;
-        
-    
-   
     }
-    
     return self;
 }
 
 -(void)encodeWithCoder:(NSCoder *)encoder {
- 
     [encoder encodeObject:self.name forKey:@"name"];
     [encoder encodeDouble:self.longitude forKey:@"longitude"];
     [encoder encodeDouble:self.latitude forKey:@"latitude"];
- 
 }
 
 -(id)initWithCoder:(NSCoder *)decoder {
     self = [super init];
     if ( self != nil ) {
-    
         self.name = [decoder decodeObjectForKey:@"name"];
         self.latitude = [decoder decodeDoubleForKey:@"longitude"];
         self.latitude = [decoder decodeDoubleForKey:@"latitude"];
-        
-        
     }
     return self;
 }
